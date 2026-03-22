7b6ea94e4a2a40707951087fc2885f91d586f9d5
Michail <michailaja@gmail.com>
2026-03-22 12:08:24 +0700

    feat: Add initial landing page component and global CSS with theme variables and utility classes.

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -6,9 +6,9 @@ import { Marquee } from "@/components/marquee";
 const MarketingPage = () => {
     return (
         <div className="min-h-full flex flex-col dark:bg-[#1F1F1F]">
+            <Marquee />
             <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
                 <Heading />
-                <Marquee />
                 <Heroes />
             </div>
             <Footer />
